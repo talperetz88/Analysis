0841538b7c2ff06952cd7fc753708d7c9b0fc1be
omri <omri@DESKTOP-G4B54CU>
2017-04-09 14:34:00 +0300

    rgb to hsv

--- AnalysisaAndEnhancementImage/src/Class/main.java
+++ AnalysisaAndEnhancementImage/src/Class/main.java
@@ -1,10 +1,29 @@
 package Class;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import GUI.*;
 public class main {
 
 	public static void main(String[] arg){
 		
-		startGUI open = new startGUI();
+		//startGUI open = new startGUI();
+		
+		//loding an image 
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("a.jpg"));
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		//
+		Image image = new Image();
+		int color = img.getRGB(0,0);
+		System.out.println(image.RGBtoH(color));
+		
 	}
 
 }
