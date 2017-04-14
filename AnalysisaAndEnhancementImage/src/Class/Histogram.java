4b2f10758c289a0801138facf05b5c4cda0dfadd
omri <omri@DESKTOP-G4B54CU>
2017-04-14 13:37:08 +0300

    all the HSV function

--- AnalysisaAndEnhancementImage/src/Class/Histogram.java
+++ AnalysisaAndEnhancementImage/src/Class/Histogram.java
@@ -10,7 +10,8 @@ public class Histogram {
 	 int[] greenBin = new int[256];
 	 int[] HBin = new int[360];
 	 int[] SBin = new int[256];
-	 int sumRed,sumGreen,sumBlue,sumS,sumH, meanRed , meanGreen , meanBlue , meanS, meanH; 
+	 int sumRed,sumGreen,sumBlue,sumS,sumH, meanRed , meanGreen , meanBlue , meanS, meanH;
+	 float hTop,redTop,greenTop,blueTop;
 	 
 	 public Histogram(BufferedImage img){
 			for(int i=0;i<img.getWidth();i++)
@@ -39,10 +40,14 @@ public class Histogram {
 				 sumGreen+=greenBin[i];  
 				 sumBlue+=blueBin[i];  
 				 sumS+=SBin[i];
-				 if(i<180)
-					 sumH+=HBin[i];
-				 
 			}
+			redTop=sumRed/256;
+			greenTop=sumGreen/256;
+			blueTop=sumBlue/256;
+				 for(int i=0;i<360;i++)
+					 	sumH+=HBin[i];
+				 hTop=sumH/360;
+			
 				meanRed=sumRed/256;
 				meanGreen=sumGreen/256;
 				meanBlue=sumBlue/256;	
@@ -91,35 +96,113 @@ public class Histogram {
 					
 			
 		}
-
-		public double IntersectionHSV(Histogram hist){
-			double sum=0;
-			for(int i=0;i<360;i++)
-				sum+=Math.min(this.HBin[i],hist.HBin[i]);
-			System.out.println("the sum"+sum+" "+this.sumH);
-			return sum/((double)(this.sumH+hist.sumH));
-			
-		}
 		
-		public double IntersectionHSVA(Histogram hist){
+		//the function is in the book page 4 
+		public double intersectionHSV(Histogram hist){
 			double sum=0;
-			for(int i=0;i<360;i++)
-				sum+=Math.abs(this.HBin[i]-hist.HBin[i]);
-			System.out.println("the sum rfdfdsfsdfdsfsdfdsfc"+sum+" the "+this.sumH);
+			for(int i=0;i<360;i++){
+				sum+=Math.min(this.HBin[i],hist.HBin[i]);
+			}
 			return sum/((double)(this.sumH));
 			
 		}
 		
-		public double IntersectionRGB(Histogram hist){
+		//the function is in the book page 4 
+		public double intersectionRGB(Histogram hist){
 			double sum=0;
 			for(int i=0;i<256;i++){
 				sum+=Math.min(this.redBin[i],hist.redBin[i]);
 				sum+=Math.min(this.greenBin[i],hist.greenBin[i]);
 				sum+=Math.min(this.blueBin[i],hist.blueBin[i]);
 			}
-			System.out.println("the sum is "+sum+" "+this.sumRed+hist.sumRed+this.sumGreen+hist.sumGreen+this.sumBlue+hist.sumBlue);
-			return sum/((double)(this.sumRed+hist.sumRed+this.sumGreen+hist.sumGreen+this.sumBlue+hist.sumBlue));
+			return sum/((double)(this.sumRed+this.sumGreen+this.sumBlue));
 			
 		}
 		
+		//the function is in the book page 4 
+		public double correlationHSV(Histogram hist){
+			
+			double sumTop=0,sumDownFirst=0,sumDownSec=0;
+			for(int i=0;i<360;i++){
+				sumTop+=((this.HBin[i]-this.hTop)*(hist.HBin[i]-hist.hTop));
+				sumDownFirst+=Math.pow((this.HBin[i]-this.hTop),2);
+				sumDownSec+=Math.pow((hist.HBin[i]-hist.hTop),2);
+			}
+			double temp = Math.sqrt((sumDownFirst*sumDownSec));
+			return sumTop/temp;
+			
+		}
+		
+		//function thats gets index , the index will indicate which component the function will calculate 1 for red 
+		//2 for blue 3 for green
+		public double helpToCorrelationRGB(int index,Histogram hist){ // צריך שם יותר טוב 
+			double sumTop=0,sumDownFirst=0,sumDownSec=0;
+			if(index==1){
+				for(int i=0;i<256;i++){
+					sumTop+=((this.redBin[i]-this.redTop)*(hist.redBin[i]-hist.redTop));
+					sumDownFirst+=Math.pow((this.redBin[i]-this.redTop),2);
+					sumDownSec+=Math.pow((hist.redBin[i]-hist.redTop),2);
+				}
+				double temp = Math.sqrt((sumDownFirst*sumDownSec));
+				return sumTop/temp;
+			}
+			if(index==2){
+				for(int i=0;i<256;i++){
+					sumTop+=((this.greenBin[i]-this.greenTop)*(hist.greenBin[i]-hist.greenTop));
+					sumDownFirst+=Math.pow((this.greenBin[i]-this.greenTop),2);
+					sumDownSec+=Math.pow((hist.greenBin[i]-hist.greenTop),2);
+				}
+				double temp = Math.sqrt((sumDownFirst*sumDownSec));
+				return sumTop/temp;
+				
+			}
+			if(index==3){
+					for(int i=0;i<256;i++){
+					sumTop+=((this.blueBin[i]-this.blueTop)*(hist.blueBin[i]-hist.blueTop));
+					sumDownFirst+=Math.pow((this.blueBin[i]-this.blueTop),2);
+					sumDownSec+=Math.pow((hist.blueBin[i]-hist.blueTop),2);
+				}
+				double temp = Math.sqrt((sumDownFirst*sumDownSec));
+				return sumTop/temp;
+			}
+			return -1; //in case of error 
+
+			
+		}
+		
+		//the function is in the book page 4 
+		public double correlationRGB(Histogram hist){
+			double [] sumComponent = new double [3];
+			double sum=0;
+			for(int j=0;j<3;j++){
+				sum+=sumComponent[j]=helpToCorrelationRGB(j+1,hist);
+			}
+			return sum/3;
+			
+		}
+		
+		//the function is in the book page 4 
+		public double chiSquareHSV(Histogram hist){
+			double sumTop=0,sumDown=0;
+			for(int i=0;i<256;i++){
+					sumTop+=(Math.pow((this.HBin[i]-hist.HBin[i]),2));
+					sumDown+=this.HBin[i]+hist.HBin[i];
+			}
+
+			return sumTop/sumDown;
+		}
+		
+		//the function is in the book page 4 
+		public double BhattacharyyaDistanceHSV(Histogram hist){
+			double sum=0;
+			double temp=(1/(Math.sqrt(this.hTop*hist.hTop*360*360)));
+			for(int i=0;i<360;i++){
+				sum+=Math.sqrt((this.HBin[i]*hist.HBin[i]));
+			}
+			if(1-(temp*sum)<=0) // in case 1-(temp*sum) is equal to zero or negative 
+				return 0;
+			else
+			return Math.sqrt(1-(temp*sum));
+		}
+
 }
--- AnalysisaAndEnhancementImage/src/Class/main.java
+++ AnalysisaAndEnhancementImage/src/Class/main.java
@@ -22,17 +22,19 @@ public class main {
 		//loding an image 
 		BufferedImage img1 = null;
 		try {
-		    img1 = ImageIO.read(new File("f.jpg"));
+		    img1 = ImageIO.read(new File("C:/Users/omri/Desktop/u.jpg"));
 		} catch (IOException e) {
 			System.err.println(e);
 		}
 		//
 		Histogram hist = new Histogram(img);
 		Histogram hist1 = new Histogram(img1);
-		System.out.println(hist.IntersectionHSV(hist1));
-		
-
-		
+		System.out.println("HSV Intersection "+hist.intersectionHSV(hist1));
+		System.out.println("RGB Intersection "+hist.intersectionRGB(hist1));
+		System.out.println("HSV Correlation "+hist.correlationHSV(hist1));
+		System.out.println("RGB Correlation "+hist.correlationRGB(hist1));
+		System.out.println("HSV chiSquare "+hist.chiSquareHSV(hist1));
+		System.out.println("HSV BhattacharyyaDistanceHSV "+hist.BhattacharyyaDistanceHSV(hist1));
 	}
 
 }
