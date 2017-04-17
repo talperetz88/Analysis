050a95a69d5ae289f9f0c297d968472fd7dceb4e
omri <omri@DESKTOP-G4B54CU>
2017-04-17 13:08:50 +0300

    all the focus function is done

--- AnalysisaAndEnhancementImage/src/Class/Focus.java
+++ AnalysisaAndEnhancementImage/src/Class/Focus.java
@@ -14,7 +14,7 @@ public class Focus {
 	
 	
 	//function that gets RGB pixel and returns the grayscale value 
-	public float grayscale(int color){
+	public float Grayscale(int color){
 		int blue = color & 0xff;
 		int green = (color & 0xff00) >> 8;
 		int red = (color & 0xff0000) >> 16;
@@ -27,6 +27,40 @@ public class Focus {
 		int green = (color & 0xff00) >> 8;
 		int red = (color & 0xff0000) >> 16;
 		return (float) ((0.2126*red)+(0.7152*green)+(0.0722*blue));
+	}
+	
+	public double MeanGrayscale(String imageName){
+		
+		double sum=0;
+		BufferedImage img = null;
+		 try {
+			 img = ImageIO.read(new File(imageName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			 for(int i =0;i<img.getWidth();i++)
+				 for(int j=0;j<img.getHeight()-1;j++){
+					 sum += Grayscale(img.getRGB(i,j));
+				 }
+			 return sum/(double)(img.getHeight()*img.getWidth());
+	}
+	
+	public double MeanLuminance(String imageName){
+		
+		double sum=0;
+		BufferedImage img = null;
+		 try {
+			 img = ImageIO.read(new File(imageName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			 for(int i =0;i<img.getWidth();i++)
+				 for(int j=0;j<img.getHeight()-1;j++){
+					 sum += Luminance(img.getRGB(i,j));
+				 }
+			 return sum/(double)(img.getHeight()*img.getWidth());
 	}
 	
 	//the function is on page 8
@@ -42,8 +76,8 @@ public class Focus {
 		}
 		 for(int i =0;i<img.getWidth();i++)
 			 for(int j=0;j<img.getHeight()-1;j++){
-					float gray1 = grayscale(img.getRGB(i,j));
-					float gray2 = grayscale(img.getRGB(i,j+1));
+					float gray1 = Grayscale(img.getRGB(i,j));
+					float gray2 = Grayscale(img.getRGB(i,j+1));
 					double temp =Math.abs(gray2-gray1);
 					if(Math.pow(temp, 2)>=v)
 						sum+=temp;
@@ -65,8 +99,8 @@ public class Focus {
 		}
 		 for(int i =0;i<img.getWidth();i++)
 			 for(int j=0;j<img.getHeight()-1;j++){
-					float gray1 = grayscale(img.getRGB(i,j));
-					float gray2 = grayscale(img.getRGB(i,j+1));
+					float gray1 = Grayscale(img.getRGB(i,j));
+					float gray2 = Grayscale(img.getRGB(i,j+1));
 					double temp = Math.pow((gray2-gray1),2);
 					if(Math.pow(temp, 2)>=v)
 						sum+=temp;
@@ -142,4 +176,72 @@ public class Focus {
 		
 	}
 	
+	
+	
+	//function 2.5.1 [7] in case the index is 1 it means grayscale , if the index is 2 it means Luminance
+	public double FocusMeasuresBasedOnImageStatisticsVariance(String imageName , int index){
+		
+		double mean,sum=0;
+		if(index==1)
+			mean=MeanGrayscale(imageName);
+		else
+			mean=MeanLuminance(imageName);
+		BufferedImage img = null;
+		 try {
+			 img = ImageIO.read(new File(imageName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 if(index==1){
+			 for(int i =0;i<img.getWidth();i++)
+				 for(int j=0;j<img.getHeight()-1;j++){
+					 sum+=Math.pow((Grayscale(img.getRGB(i,j)))-(mean), 2);
+				 
+				 	}
+		 }
+		 else
+			 for(int i =0;i<img.getWidth();i++)
+				 for(int j=0;j<img.getHeight()-1;j++){
+					 sum+=Math.pow((Luminance(img.getRGB(i,j)))-(mean), 2);
+				 
+				 	}
+		
+		return sum/(double)(img.getHeight()*img.getWidth());
+		
+	}
+	
+	//function 2.5.1 [8] in case the index is 1 it means grayscale , if the index is 2 it means Luminance
+	public double FocusMeasuresBasedOnImageStatisticsNormalizedVariance(String imageName , int index){
+		double mean,sum=0;
+		if(index==1)
+			mean=MeanGrayscale(imageName);
+		else
+			mean=MeanLuminance(imageName);
+		BufferedImage img = null;
+		 try {
+			 img = ImageIO.read(new File(imageName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 if(index==1){
+			 for(int i =0;i<img.getWidth();i++)
+				 for(int j=0;j<img.getHeight()-1;j++){
+					 sum+=Math.pow((Grayscale(img.getRGB(i,j)))-(mean), 2);
+				 
+				 	}
+		 }
+		 else
+			 for(int i =0;i<img.getWidth();i++)
+				 for(int j=0;j<img.getHeight()-1;j++){
+					 sum+=Math.pow((Luminance(img.getRGB(i,j)))-(mean), 2);
+				 
+				 	}
+		
+		return sum/((double)(img.getHeight()*img.getWidth())*mean);
+		
+	}
+
+
 }
