3512a21c44cdf309909871d995610a26acc76750
omri <omri@DESKTOP-G4B54CU>
2017-05-29 13:26:07 +0300

    s

--- AnalysisaAndEnhancementImage/src/Test/ImageSimilarity.java
+++ AnalysisaAndEnhancementImage/src/Test/ImageSimilarity.java
@@ -20,24 +20,6 @@ import GUI.classifyImages;
 
 public class ImageSimilarity {
 	
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void test() {
 		CUtils.SetImagesDestinationPath("C:\\project\\testImageSimilarity\\");
