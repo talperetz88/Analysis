8ab3d41c8952b446ac5e4c499404ec8d76ac6ce4
omri <omri@DESKTOP-G4B54CU>
2017-05-29 13:21:17 +0300

    text image similarity

--- AnalysisaAndEnhancementImage/src/Test/ImageSimilarity.java
+++ AnalysisaAndEnhancementImage/src/Test/ImageSimilarity.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.*;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -43,10 +47,22 @@ public class ImageSimilarity {
 		c.intersectionFunc();
 		c.bhattFunc();
 		c.chiSquareFunc();*/
-		c.correlationFunc();
-		
+		boolean bool = true;
+		boolean temp;
+		File folder = new File(CUtils.GetImagesDestPath() +"Histogram\\Correlation\\RGB\\1");
+		String name=".png";
+		File[] listOfFiles = folder.listFiles();
+		for (int i = 0; i < listOfFiles.length -1; i++){
+			String n = (i+1)+name;
+			temp=n.equals(listOfFiles[i].getName());
+			if (temp==false)
+				bool = false;
+		Assert.assertTrue(bool);
+
 		}
+	}
+}
 		
 
 
-}
+
